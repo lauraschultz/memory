ce96536cb4ec4878065c77aa4c472c78335c1d24
lauraschultz <schultzlaurac@gmail.com>
2020-06-14 15:48:45 -0500

    Updates

---
+++ precache-manifest.b09895ac383cf9009ad0b4a996e25df4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34491d85234eedf0ba87bcf288d4f389",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "b1e0bbcae74fa5db6f1c",
+    "url": "/memory/static/css/main.c4be4343.chunk.css"
+  },
+  {
+    "revision": "830024a88814bb05fd39",
+    "url": "/memory/static/js/2.4183dfaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory/static/js/2.4183dfaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e0bbcae74fa5db6f1c",
+    "url": "/memory/static/js/main.9e55df6b.chunk.js"
+  },
+  {
+    "revision": "7ed0e48e27aa87b00c38",
+    "url": "/memory/static/js/runtime-main.31103d8b.js"
+  },
+  {
+    "revision": "826fa561abcdc3a450a949fc7ea20fbf",
+    "url": "/memory/static/media/apple.826fa561.png"
+  },
+  {
+    "revision": "209ca224312785ed70f3be69444abb8d",
+    "url": "/memory/static/media/banana.209ca224.png"
+  },
+  {
+    "revision": "6dfc715813edb767fd50123807fa32b2",
+    "url": "/memory/static/media/bell-pepper.6dfc7158.png"
+  },
+  {
+    "revision": "bb9304a30694c595ff43c3cdc62d0c2a",
+    "url": "/memory/static/media/brocolli.bb9304a3.png"
+  },
+  {
+    "revision": "ab883e54fd22cde44c5555c5c8f69284",
+    "url": "/memory/static/media/carrot.ab883e54.png"
+  },
+  {
+    "revision": "6e7e899233e741071629fd034c1d59ed",
+    "url": "/memory/static/media/denim.6e7e8992.png"
+  },
+  {
+    "revision": "2c6cd0e313ac9c7cf4a4e04c10a797b6",
+    "url": "/memory/static/media/eggplant.2c6cd0e3.png"
+  },
+  {
+    "revision": "eea2312d810d6d24ff570260df2dc109",
+    "url": "/memory/static/media/grapes.eea2312d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b07d0d5f02b16d2459e7dd7157e62e63",
+    "url": "/memory/static/media/mushroom.b07d0d5f.png"
+  },
+  {
+    "revision": "8779b99f7e69f61b2654926ba6809619",
+    "url": "/memory/static/media/onion.8779b99f.png"
+  },
+  {
+    "revision": "287f220e6da61fd45ec973368a27e699",
+    "url": "/memory/static/media/pear.287f220e.png"
+  },
+  {
+    "revision": "065e665e94fa84cdacec1bc298b0315e",
+    "url": "/memory/static/media/pineapple.065e665e.png"
+  },
+  {
+    "revision": "3dfa01f04d270f34bf0ddf33817198e9",
+    "url": "/memory/static/media/red-pepper.3dfa01f0.png"
+  },
+  {
+    "revision": "d3364fb58f74c1f8a820a180b6f8a9d4",
+    "url": "/memory/static/media/strawberry.d3364fb5.png"
+  },
+  {
+    "revision": "0025f2fdf0ee5081ca923fb2af27138b",
+    "url": "/memory/static/media/tomato.0025f2fd.png"
+  },
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/memory/static/media/white-waves.a06a3248.png"
+  }
+]);
